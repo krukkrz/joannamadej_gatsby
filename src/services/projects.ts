98af60559059e782a61a04914a49d98ee62e1df5
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-10-30 14:51:30 +0100

    tiles added XXL

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,12 +1,30 @@
+//@ts-ignore
 import placeholder from "../assets/my_projects_tiles/placeholder.svg";
+//@ts-ignore
 import omantelXL from "../assets/projects/omantel/omantel_xl.svg";
+//@ts-ignore
 import omantelXXL from "../assets/projects/omantel/omantel_xxl.svg";
+//@ts-ignore
 import guildXL from "../assets/projects/guild/guild_xl.svg";
+//@ts-ignore
 import guildXXL from "../assets/projects/guild/guild_xxl.svg";
+//@ts-ignore
 import desklyXL from "../assets/projects/deskly/deskly_xl.svg";
+//@ts-ignore
 import desklyXXL from "../assets/projects/deskly/deskly_xxl.svg";
+//@ts-ignore
 import healthymeXL from "../assets/projects/healthyme/healthyme_xl.svg";
+//@ts-ignore
 import healthymeXXL from "../assets/projects/healthyme/healthyme_xxl.svg";
+
+//@ts-ignore
+import deskly_tile from "../assets/my_projects_tiles/deskly_tile.png";
+//@ts-ignore
+import guild_tile from "../assets/my_projects_tiles/guild_tile.png";
+//@ts-ignore
+import healthyme_tile from "../assets/my_projects_tiles/healthyme_tile.png";
+//@ts-ignore
+import omantel_tile from "../assets/my_projects_tiles/omantel_tile.png";
 
 export const getProjects = (): Project[] => {
   return [
@@ -15,7 +33,7 @@ export const getProjects = (): Project[] => {
       title: "Guild",
       description:
         "The guild app is a social app created to make networking easier. While working at Monterail Software House, I had the opportunity to design new features for the Guild app and redesign old ones.",
-      tile: placeholder,
+      tile: guild_tile,
       url: "/guild",
       heroXL: guildXL,
       heroXXL: guildXXL,
@@ -26,7 +44,7 @@ export const getProjects = (): Project[] => {
       title: "Omantel online store",
       description:
         "Omantel is the largest telecommunications company in Oman. In addition to telecommunications services, they also have an online store with electronics. While working at the Unriddl agency, I had the opportunity to work on the Omantel store project and design new features for it.",
-      tile: placeholder,
+      tile: omantel_tile,
       heroXL: omantelXL,
       heroXXL: omantelXXL,
       url: "/omantel",
@@ -37,7 +55,7 @@ export const getProjects = (): Project[] => {
       title: "Deskly",
       description:
         "UX case study, created to showcase my design process and UX skills. As part of the project, I conducted a UX audit and in-depth interviews with users, and I designed screens for Deskly - a desk booking application.",
-      tile: placeholder,
+      tile: deskly_tile,
       url: "/deskly",
       heroXL: desklyXL,
       heroXXL: desklyXXL,
@@ -48,7 +66,7 @@ export const getProjects = (): Project[] => {
       title: "HealthyMe",
       description:
         "HealthyMe is an app that aims to support users in implementing healthy habits into their daily routines. I created an interface design for it based on the provided lo-fi mockups. Designs were made for the Dare IT UI Challenge competition in which I recived an honorable mention.",
-      tile: placeholder,
+      tile: healthyme_tile,
       url: "/healthyme",
       heroXL: healthymeXL,
       heroXXL: healthymeXXL,
