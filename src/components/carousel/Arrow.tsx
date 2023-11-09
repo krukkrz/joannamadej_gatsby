0513c338e33521da6c24c1b53bcea0af41abffdc
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-11-09 19:04:22 +0100

    carousel: Hide arrows on small screens

--- src/components/carousel/Arrow.tsx
+++ src/components/carousel/Arrow.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import buttonLeft from "../../assets/icons/button_left.png";
 // @ts-ignore
 import buttonRight from "../../assets/icons/button_right.png";
+// @ts-ignore
+import * as styles from "./Arrow.module.scss";
 
 type ArrowProps = {
     className: string;
@@ -12,18 +14,25 @@ type ArrowProps = {
     onClick: any;
 }
 
-const Arrow = ({ style, className, direction, onClick }: ArrowProps) => (
-        <img
-            className={className}
-            style={{
-                ...style, display: "block",
-                height: "auto",
-                width: "auto",
-            }}
-            onClick={onClick}
-            src={direction === "left" ? buttonLeft : buttonRight}
-            alt="buttonLeft" />
-);
+const Arrow = ({ style, className, direction, onClick }: ArrowProps) => {
+    console.log(className)
+
+    return (
+        <div className={styles.arrow}>
+            <img
+                className={className}
+                style={{
+                    ...style,
+                    display: "block",
+                    height: "auto",
+                    width: "auto",
+                }}
+                onClick={onClick}
+                src={direction === "left" ? buttonLeft : buttonRight}
+                alt="buttonLeft"/>
+        </div>
+    );
+}
 
 type Direction = "left" | "right";
 
