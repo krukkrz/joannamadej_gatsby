bc8dadb7d38ab82a9399fdc20c7af8f362e5e425
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-12-11 17:09:03 +0100

    about_me: text update

--- src/components/about_me/AboutMe.tsx
+++ src/components/about_me/AboutMe.tsx
@@ -9,14 +9,13 @@ const AboutMe = () => {
       <p>
         I am a UX/UI Designer with 2 years of experience. I have designed new
         features for the Guild app and Omantel online store and together with
-        Senior Product Designer I have redesigned the internal analytical
-        platform for one of three top consulting firms.
+        another designer I have redesigned the internal analytical
+        platform for consulting firm.
       </p>
       <p>
         I have experience in developing wireframes, and prototypes, working with
-        design systems, and conducting usability tests and UX audits. I am
-        passionate about making users' lives easier by improving the products
-        they use.{" "}
+        design systems, and conducting usability tests and UX audits.
+        I am passionate about making users lives easier by improving the products they use, and I am always eager to learn new tools and techniques to enhance my design skills and knowledge.
       </p>
       <p>
         I'm open to new challenges. If you're looking for someone to work on a
